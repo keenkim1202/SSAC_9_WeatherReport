296869d37808882e9accfea35a99804cb8b60910
KEEN <homurahomu@naver.com>
2021-10-25 16:56:47 +0900

    APIKEY 설정 및 api호출test 코드작성

---
+++ SSAC_9_WeatherReport/APIKEY.swift
@@ -0,0 +1,12 @@
+//
+//  APIKEY.swift
+//  SSAC_9_WeatherReport
+//
+//  Created by KEEN on 2021/10/25.
+//
+
+import Foundation
+
+struct APIKEY {
+  static let apikey = "APIKEY를 입력해주세요"
+}
--- SSAC_9_WeatherReport/WeatherViewController.swift
+++ SSAC_9_WeatherReport/WeatherViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import CoreLocation
+
+// TODO: APIKEY 파일안에 apikey를 작성해주세요.
 
 class WeatherViewController: UIViewController {
-
+  
+  // MARK: - Properties
+  let locationManager = CLLocationManager()
+  
+  var latitude: Double?
+  var longitude: Double?
+  
   // MARK: - UI
   @IBOutlet weak var dateLabel: UILabel!
   @IBOutlet weak var locationLabel: UILabel!
@@ -25,8 +34,9 @@ class WeatherViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setLabel([temperatureLabel, humidityLabel, windSpeedLabel, feelingLabel])
+    getCurrentWeather(37.65469539112308, 127.0605780377212)
   }
-
+  
   // MARK: - Configure
   func setLabel(_ labels: [PaddingLabel]) {
     for l in labels {
@@ -34,7 +44,20 @@ class WeatherViewController: UIViewController {
       l.layer.cornerRadius = CGFloat(8)
     }
   }
-
+  
+  func getCurrentWeather(_ latitude: Double, _ longitude: Double) {
+    let url = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(APIKEY.apikey)"
+    AF.request(url, method: .get).validate().responseJSON { response in
+      switch response.result {
+      case .success(let value):
+        let json = JSON(value)
+        print("JSON: \(json)")
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
+  
   // MARK: - Actions
   @IBAction func onShareButton(_ sender: UIButton) {
   }
@@ -44,3 +67,7 @@ class WeatherViewController: UIViewController {
   
 }
 
+// MARK: Extension - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate {
+  
+}
